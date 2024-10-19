624fefe2727a45f4654dac1a0c0e9d22fb1766bf
KM-joel <joelkabamba35@gmail.com>
2024-10-19 20:33:59 +0100

    ADD: import test csv

---
+++ import_csv/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    "name": "HLP import attribute",
+    "summary": "Module for import attribute line",
+    "author": "KARIZMA Conseil (KZM)",
+    "website": "https://karizma-conseil.com/",
+    "category": "Product",
+    "license": "LGPL-3",
+    "version": "17.0.0.0.0",
+    "depends": ["product"],
+    "data": ['data/action_import_variant_cron.xml'],
+}
