1b31e417ac219fc408ce1e4049fc3a8af2b16665
KM-joel <joelkabamba35@gmail.com>
2024-10-21 16:02:00 +0100

    [UPD] import csv

--- import_attribute_variant/models/product.py
+++ import_attribute_variant/models/product.py
@@ -12,36 +12,43 @@ class ProductAttributeLine(models.Model):
         csv_file = '/opt/Copie_de_attributes_data_1.csv'
         try:
             with open(csv_file, mode='r', encoding='utf-8') as file:
-                reader = csv.DictReader(file, delimiter=';')
+                reader = csv.DictReader(file, delimiter=',')
                 for row in reader:
                     modified_row = {}
                     for key, value in row.items():
-                        if key == 'ID externe':
-                            modified_row['product_tmpl_id'] = self.env.ref(str(value))
-                        if key == 'attribute_line_ids/id':
-                            modified_row['line'] = self.env.ref(str(value))
+                        if key == 'id externe':
+                            val = value.split('_')
+                            prod_templ = self.env['product.template'].search([
+                                ('active', '=?', False), ('id_externe', '=', val[1])
+                            ])
+                            modified_row['product_tmpl_id'] = prod_templ  # self.env.ref(str(value))
+
                         if key == 'attribute_line_ids/attribute_id/id':
                             modified_row['attribute_id'] = self.env.ref(str(value))
                         if key == 'attribute_line_ids/value_ids/id':
                             modified_row['value_ids'] = self.env.ref(str(value))
 
-                    if modified_row.get('product_tmpl_id') and modified_row.get('line') and modified_row.get('attribute_id') and modified_row.get('value_ids'):
-                        product_tmpl_id = self.env['product.template'].browse(modified_row.get('product_tmpl_id').id)
-                        _logger.error(f"=>>>>> product name : {product_tmpl_id.name}")
-                        domain = [
-                            ('id', '=', modified_row.get('line').id),
-                            ('attribute_id', '=', modified_row.get('attribute_id').id)
-                        ]
-                        if not product_tmpl_id.attribute_line_ids or not product_tmpl_id.attribute_line_ids.search(domain):
+                    if modified_row.get('product_tmpl_id') and modified_row.get('attribute_id') and modified_row.get('value_ids'):
+                        product_tmpl_id = modified_row.get('product_tmpl_id')
+                        # domain = [('attribute_id', '=', modified_row.get('attribute_id').id)]
+                        lines = product_tmpl_id.attribute_line_ids.filtered(lambda li: li.attribute_id.id == modified_row.get('attribute_id').id)
+                        if product_tmpl_id.attribute_line_ids and lines:
+                            _logger.info(f"=>>>>> Exist values : {product_tmpl_id.attribute_line_ids.mapped('value_ids')}")
+                            for line in lines:
+                                line.value_ids = [Command.link(modified_row.get('value_ids').id)]
+                            _logger.info(f"=>>>>> product name : {product_tmpl_id.name}")
+                            _logger.info(f"=>>>>> Exist values : {product_tmpl_id.attribute_line_ids.mapped('value_ids')}")
+
+                        elif not product_tmpl_id.attribute_line_ids or not lines:
                             vals = {
+                                'product_tmpl_id': product_tmpl_id.id,
                                 'attribute_id': modified_row.get('attribute_id').id,
                                 'value_ids': [Command.set([modified_row.get('value_ids').id])],
                             }
+                            _logger.info(f"=>>>>> line : {product_tmpl_id.attribute_line_ids}")
                             product_tmpl_id.attribute_line_ids = [Command.create(vals)]
-
-                        if product_tmpl_id.attribute_line_ids and product_tmpl_id.attribute_line_ids.search(domain):
-                            for line in product_tmpl_id.attribute_line_ids.search(domain):
-                                line.value_ids = [Command.link(modified_row.get('value_ids').id)]
+                            _logger.info(f"=>>>>> product name : {product_tmpl_id.name}")
+                            _logger.info(f"=>>>>> line : {product_tmpl_id.attribute_line_ids}")
 
         except Exception as exc:
             _logger.error(f"=>>>>> : {exc}")
